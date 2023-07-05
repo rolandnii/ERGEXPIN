87ff8c01d5441648ce4fabdee342bc00e9eed990
rolandnii <rolandniidodoo@gmail.com>
2023-07-05 23:18:27 +0000

    Run all mimgrations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
                 <div class="content-wrapper">
                     {{ $slot }}
                 </div>
-                {{-- @include('includes.footer') --}}
+                @include('includes.footer')
             </div>
         </div>
     </div>
