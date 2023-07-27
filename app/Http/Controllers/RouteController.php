f112887be7747bc1649410dd75056c3f124bd069
rolandnii <rolandniidodoo@gmail.com>
2023-07-27 18:16:36 +0000

    Latest expenses and stats done

--- app/Http/Controllers/RouteController.php
+++ app/Http/Controllers/RouteController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Expense;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Query\Builder;
 
 class RouteController extends Controller
 {
-    public function __construct() {
-        
+    public function __construct()
+    {
     }
 
     public function ShowDashboard()
@@ -17,7 +20,39 @@ class RouteController extends Controller
 
     public function ShowExpense()
     {
-        return view("modules.normal.expense.index");
+
+        
+
+
+        $query = Expense::query();
+        $latestExpenses = $query->join("expense_types", "expenses.exp_type", "expense_types.id")
+            ->select(
+                "expenses.*",
+                "expense_types.label",
+                "expense_types.icon",
+            )
+            ->where("expenses.user_id",auth()->user()->id)
+            ->orderBy("expenses.created_at","desc")
+            ->limit(4)
+            ->get();
+
+        $totalamount = $query->where("expenses.user_id",auth()->user()->id)
+        ->sum("exp_amount");
+        $stats = DB::table("expenses")->join("expense_types", "expenses.exp_type", "expense_types.id")
+        ->select(
+            DB::raw("SUM(expenses.exp_amount) as  amount" ),
+            "expense_types.label",
+            // "expense_types.icon",
+
+        )
+        ->where("expenses.user_id",auth()->user()->id)
+        ->orderBy("amount","desc")
+        ->groupBy("expense_types.label")
+        ->limit(4)
+        ->get()
+        ;
+        
+        return view("modules.normal.expense.index" ,compact('latestExpenses',"stats","totalamount"));
     }
 
     public function ShowAddExpense()
--- resources/views/modules/normal/expense/index.blade.php
+++ resources/views/modules/normal/expense/index.blade.php
@@ -40,23 +40,30 @@
                 <div class="card-body">
                     <h3 class="text-lg text-gray-700 font-semibold mb-2"> Expenses Statistics</h3>
 
-                    <div class="w-full mb-3">
+                    @foreach ($stats as $stat)
+                    @php
+                    $percent = ($stat->amount/ $totalamount) * 100;
+
+                    // ddd(round($percent));
+                    @endphp
+                    <div class="w-full mb-4 ">
                         <div class="flex items-center gap-2 w-full mb-2">
                             <div
                                 class="bg-blue-100 text-primary font-bold w-10 h-10 text-lg flex items-center justify-center rounded-full">
                                 U</div>
-                            <div class="text-base">Utility bills</div>
+                            <div class="text-base">{{ $stat->label }}</div>
                         </div>
 
-                        <div class="progress mb-1">
-                            <div class="progress-bar w-50 bg-primary" role="progressbar" aria-label="Basic example"
-                                aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                        <div class="progress mb-2">
+                            <div class="progress-bar bg-primary" role="progressbar"  style="width: {{ $percent }}%"
+                                aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                         </div>
                         <div class="flex justify-between items-center">
-                            <span>$1,200,000</span>
-                            <span>50%</span>
+                            <span>${{ $stat->amount }}</span>
+                            <span>{{ round($percent) }}%</span>
                         </div>
                     </div>
+                    @endforeach
 
                     {{-- <div class="w-full">
                         <div class="flex items-center gap-2 w-full mb-2">
@@ -82,32 +89,23 @@
             <div class="card">
                 <div class="card-body text-gray-700">
                     <h3 class="text-lg text-gray-700 font-semibold mb-3">Latest Expenses</h3>
+                    @foreach ($latestExpenses as $expense)
                     <div class="bg-light p-4 rounded mb-3">
                         <div class="flex items-center justify-between">
                             <div class="flex gap-3">
                                 <div class="bg-warning pt-1 flex items-center justify-center h-10 rounded-full w-10"><i
-                                        class="mdi mdi-archive text-gray-50"></i></div>
+                                        class="mdi mdi-{{ $expense->icon }} text-gray-50"></i></div>
                                 <div>
-                                    <div>Monday cruise</div>
-                                    <div class="text-sm text-gray-400">Health</div>
+                                    <div>{{ $expense->exp_title }}</div>
+                                    <div class="text-sm text-gray-400">{{ $expense->label }}</div>
                                 </div>
                             </div>
-                            <span class="text-danger">-$1000</span>
+                            <span class="text-danger">-${{ $expense->exp_amount }}</span>
                         </div>
                     </div>
-                    <div class="bg-light p-4 rounded mb-3">
-                        <div class="flex items-center justify-between">
-                            <div class="flex gap-3">
-                                <div class="bg-warning pt-1 flex items-center justify-center h-10 rounded-full w-10"><i
-                                        class="mdi mdi-archive text-gray-50"></i></div>
-                                <div>
-                                    <div>Monday cruise</div>
-                                    <div class="text-sm text-gray-400">Health</div>
-                                </div>
-                            </div>
-                            <span class="text-danger">-$1000</span>
-                        </div>
-                    </div>
+                
+                    @endforeach
+                    
 
                 </div>
             </div>
